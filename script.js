f369ae0b580d5228f6e2997052944b5efb875e19
Buddhika Thanura <buddhikajayasingha@gmail.com>
2023-02-27 19:00:12 +0530

    Update script.js
    Updated the code to enhance the emoji explosion feature by adding the uniqueness option to the emojisplosion function. This ensures that the location of the explosion is not repeated too frequently in the explosion. Additionally, increased the number of times the function runs to create more explosions. This change improves the visual effect of the feature."

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // defining variables
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
@@ -7,15 +7,15 @@ let highscore = 0;
 
 // Functions for change message text
 const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+  document.querySelector(".message").textContent = message;
 };
 
 //Function to change bg color when loosing
 const loosingColors = function (score) {
-  const colors = ['#f80000', '#ba0000', '#7c0000', '#3e0000', '#222'];
+  const colors = ["#f80000", "#ba0000", "#7c0000", "#3e0000", "#222"];
   for (let i = 0; i < colors.length; i++) {
     if (score === i + 1) {
-      document.querySelector('body').style.backgroundColor = colors[i];
+      document.querySelector("body").style.backgroundColor = colors[i];
       break; // exit the loop once the condition is met
     }
   }
@@ -23,47 +23,50 @@ const loosingColors = function (score) {
 
 // Function to flash the screen when losing
 const flashScreen = function () {
-  const body = document.querySelector('body');
-  if (body.style.backgroundColor === 'red') {
-    body.style.backgroundColor = '#222';
+  const body = document.querySelector("body");
+  if (body.style.backgroundColor === "red") {
+    body.style.backgroundColor = "#222";
   } else {
-    body.style.backgroundColor = 'red';
+    body.style.backgroundColor = "red";
   }
 };
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
 
   // When there is no input
   if (!guess) {
-    displayMessage('⛔️ No Number!');
+    displayMessage("⛔️ No Number!");
 
     // When player wins
   } else if (guess == secretNumber) {
-    displayMessage('🎉 Correct Number!');
-    document.querySelector('.number').textContent = secretNumber;
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
     // chnaging body color when user wins. also increase secret number box width
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
     //adding emoji explosion -- code by JoshuaKGoldberg
-    emojisplosion({
-      emojis: ['🎉', '🎊'],
-    });
+    for (let i = 0; i < 3; i++) {
+      emojisplosion({
+        emojis: ["🎉", "🎊"],
+        uniqueness: 1,
+      });
+    }
 
     //Highscore condition
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      document.querySelector(".highscore").textContent = highscore;
     }
     // When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
       score--;
-      document.querySelector('.score').textContent = score;
+      document.querySelector(".score").textContent = score;
     } else {
-      displayMessage('💥 You lost the game!');
-      document.querySelector('.score').textContent = 0;
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
       // Flash the screen when the player loses
       const flashing = setInterval(flashScreen, 100);
       setTimeout(function () {
@@ -74,15 +77,15 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 
   //defining Reset
-  document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector(".again").addEventListener("click", function () {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-    document.querySelector('.score').textContent = score;
-    document.querySelector('.number').textContent = '?';
-    displayMessage('Start guessing...');
-    document.querySelector('.guess').value = '';
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?";
+    displayMessage("Start guessing...");
+    document.querySelector(".guess").value = "";
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
   });
 });
